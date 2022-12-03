07c1810c60937569c5b672c6c67999a0b1484e7f
thedoru <thedoruv2@gmail.com>
2022-12-03 13:15:04 +0100

    Update Controllers

--- controllers/authorController.js
+++ controllers/authorController.js
@@ -1,11 +1,52 @@
+const async = require("async");
 const Author = require("../models/author");
+const Book = require("../models/book");
+const { ObjectId } = require("mongodb");
 
-exports.author_list = (req, res) => {
-  res.send("NOT IMPLEMENTED: Author list");
+exports.author_list = (req, res, next) => {
+  Author.find()
+    .sort([["family_name", "ascending"]])
+    .exec(function (err, list_authors) {
+      if (err) {
+        return next(err);
+      }
+
+      res.render("author_list", {
+        title: "Author List",
+        author_list: list_authors,
+      });
+    });
 };
 
-exports.author_detail = (req, res) => {
-  res.send(`NOT IMPLEMENTED: Author detail: ${req.params.id}`);
+exports.author_detail = (req, res, next) => {
+  async.parallel(
+    {
+      author(callback) {
+        Author.findById(req.params.id).exec(callback);
+      },
+      authors_books(callback) {
+        const _id = ObjectId(req.params.id);
+        Book.find({ author: _id }).exec(callback);
+      },
+    },
+    (err, results) => {
+      if (err) {
+        return next(err);
+      }
+
+      if (results.author == null) {
+        const err = new Error("Author not found");
+        err.status = 404;
+        return next(err);
+      }
+
+      res.render("author_detail", {
+        title: "Author Detail",
+        author: results.author,
+        author_books: results.authors_books,
+      });
+    }
+  );
 };
 
 exports.author_create_get = (req, res) => {
